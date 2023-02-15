c1e8e1d4b9d065f84a3582cc7793db6157950e68
Adilma Alves <125414087+AdilmaAlves@users.noreply.github.com>
2023-02-14 21:21:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double nota1,nota2,nota3,media;
+
+Console.WriteLine("Digite a 1° nota:");
+nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a 2° nota:");
+nota2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a 3° nota:");
+nota3 = double.Parse(Console.ReadLine());
+
+media = (nota1+nota2+nota3)/3;
+
+if(media >= 6){
+    Console.WriteLine($"Aluno Aprovado com média: {media}");
+}
+if(media >= 5 && media < 6){
+    Console.WriteLine($"Aluno em Recuperação com média: {media}");
+}
+if(media < 5){
+    Console.WriteLine($"Aluno Reprovado com média: {media}");
+}
